484504f6ea1944613fa347e73d7252ed75060951
Felipe <fperezferraro@cdt.com.ar>
2024-12-09 15:25:38 -0300

    Creo el servicio de mis productos

---
+++ src/app/products/service/products.service.ts
@@ -0,0 +1,20 @@
+import { inject, Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import {
+  Motorbike,
+  MotorbikeResponse,
+} from '../interfaces/motorbikes.interface';
+import { HttpClient } from '@angular/common/http';
+
+const URL = 'http://localhost:3000/motorbikes?_page=1&_per_page=10';
+
+@Injectable({ providedIn: 'root' })
+export class ProductsService {
+  private htpp = inject(HttpClient);
+
+  getBannerMotorbikes(): Observable<Motorbike[]> {
+    return this.htpp
+      .get<MotorbikeResponse>(URL)
+      .pipe(map((res) => res.data.splice(0, 5)));
+  }
+}
